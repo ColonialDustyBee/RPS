96849b38d1cf38e06f741609e1aeac753ebff85c
Jesus <111938653+ColonialDustyBee@users.noreply.github.com>
2022-10-16 22:18:32 -0700

    Added MessageBox prompts.

--- RPS.cpp
+++ RPS.cpp
@@ -1,4 +1,5 @@
 #include "RPS.h"
+#include <Windows.h>
 #include <iostream>
 
 RPS::RPS() { }; // Maybe constructor should make it so
@@ -18,34 +19,35 @@ void RPS::initializePlayer2(int input) {
 }
 
 void RPS::whosTheWinner() {
+	// Lets do this pretty thing, shall we?
 	std::cout << "Player 1 chose: " << choices[player1] << std::endl;
 	std::cout << "Player 2 chose: " << choices[player2] << std::endl;
 	if(player1 == player2){ // If there's a tie
-		std::cout << "It's a tie!, no one wins!" << std::endl;
+		MessageBox(NULL, L"No one wins!", L"It's a tie!", MB_OK);
 		return;
 	}
 	if (player1 == static_cast<int>(diffChoices::ROCK) && player2 == static_cast<int>(diffChoices::SCISSORS)) { // 0 and 2
-		std::cout << "Player 1 wins!" << std::endl; 
+		MessageBox(NULL, L"Player 1 Wins!", L"Who's the winner!", MB_OK);
 		return;
 	}
 	if(player1 == static_cast<int>(diffChoices::SCISSORS) && player2 == static_cast<int>(diffChoices::ROCK)) { // 2 and 0
-		std::cout << "Player 2 wins!" << std::endl;
+		MessageBox(NULL, L"Player 2 Wins!", L"Who's the winner!", MB_OK);
 		return;
 	}
 	if (player1 == static_cast<int>(diffChoices::SCISSORS) && player2 == static_cast<int>(diffChoices::PAPER)) { //2 and 1
-		std::cout << "Player 1 wins!" << std::endl;
+		MessageBox(NULL, L"Player 1 Wins!", L"Who's the winner!", MB_OK);
 		return;
 	}
 	if(player1 == static_cast<int>(diffChoices::PAPER) && player2 == static_cast<int>(diffChoices::SCISSORS)) { // 1 and 2
-		std::cout << "Player 2 wins!" << std::endl;
+		MessageBox(NULL, L"Player 2 Wins!", L"Who's the winner!", MB_OK);
 		return;
 	}
 	if (player1 == static_cast<int>(diffChoices::PAPER) && player2 == static_cast<int>(diffChoices::ROCK)) { // 1 and 0
-		std::cout << "Player 1 wins!" << std::endl;
+		MessageBox(NULL, L"Player 1 Wins!", L"Who's the winner!", MB_OK);
 		return;
 	}
 	if (player1 == static_cast<int>(diffChoices::ROCK) && player2 == static_cast<int>(diffChoices::PAPER)) { // 0 and 1
-		std::cout << "Player 2 wins!" << std::endl;
+		MessageBox(NULL, L"Player 2 Wins!", L"Who's the winner!", MB_OK);
 		return;
 	}
 }
